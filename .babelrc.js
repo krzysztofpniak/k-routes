6f3828c7bf75f6cd445d87810beefb1bc05665b7
Krzysztof Pniak <krzysztof.pniak@cardinality.co.uk>
2018-11-29 20:20:00 +0100

    style(.babelrc): reformat with prettier

--- .babelrc.js
+++ .babelrc.js
@@ -1,34 +1,35 @@
-const { NODE_ENV } = process.env;
+const {NODE_ENV} = process.env;
 
 module.exports = {
-    presets: [
-        [
-            '@babel/env',
-            {
-                targets: {
-                    browsers: ['ie >= 11']
-                },
-                exclude: ['transform-async-to-generator', 'transform-regenerator'],
-                modules: false,
-                loose: true
-            }
-        ],
-        [
-            '@babel/react',
-            {
-                targets: {
-                    browsers: ['ie >= 11']
-                },
-                exclude: ['transform-async-to-generator', 'transform-regenerator'],
-                modules: false,
-                loose: true
-            }
-        ]
+  presets: [
+    [
+      '@babel/env',
+      {
+        targets: {
+          browsers: ['ie >= 11'],
+        },
+        exclude: ['transform-async-to-generator', 'transform-regenerator'],
+        modules: false,
+        loose: true,
+      },
     ],
-    plugins: [
-        // don't use `loose` mode here - need to copy symbols when spreading
-        '@babel/proposal-object-rest-spread',
-        '@babel/plugin-transform-spread',
-        NODE_ENV === 'test' && '@babel/transform-modules-commonjs'
-    ].filter(Boolean)
+    [
+      '@babel/react',
+      {
+        targets: {
+          browsers: ['ie >= 11'],
+        },
+        exclude: ['transform-async-to-generator', 'transform-regenerator'],
+        modules: false,
+        loose: true,
+      },
+    ],
+  ],
+  plugins: [
+    // don't use `loose` mode here - need to copy symbols when spreading
+    '@babel/plugin-transform-template-literals',
+    '@babel/proposal-object-rest-spread',
+    '@babel/plugin-transform-spread',
+    NODE_ENV === 'test' && '@babel/transform-modules-commonjs',
+  ].filter(Boolean),
 };
