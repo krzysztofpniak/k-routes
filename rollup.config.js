ae8809d40a078252d601fe8b9f65ab3f259b443f
Krzysztof Pniak <krzysztof.pniak@cardinality.co.uk>
2018-11-29 20:22:10 +0100

    style(rollup): reformat with prettier

--- rollup.config.js
+++ rollup.config.js
@@ -5,44 +5,58 @@ import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
 export default [
-	// browser-friendly UMD build
-	{
-		input: 'src/main.js',
-		output: {file: pkg.browser, format: 'umd', name: 'k-routes'},
-		plugins: [
-            replace({
-                "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
-            }),
-            babel({
-                exclude: ['node_modules/**']
-            }),
-            nodeResolve({
-                jsnext: true,
-            }),
-            commonjs({
-                namedExports: {
-                    'node_modules/react/index.js': ['createFactory', 'Children', 'Component', 'PropTypes', 'createElement', 'cloneElement'],
-                    'node_modules/react-dom/index.js': ['render']
-                }
-			}),
-		],
-	},
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// the `targets` option which can specify `dest` and `format`)
-	{
-		input: 'src/main.js',
-		external: ['ramda', 'react', 'reselect', 'k-reducer', 'redux', 'recompose', 'redux-actions', 'path-parser', 'react-redux'],
-		output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' }
-		],
-		plugins: [
-			babel({
-				exclude: ['node_modules/**']
-			})
-		]
-	}
+  // browser-friendly UMD build
+  {
+    input: 'src/main.js',
+    output: {file: pkg.browser, format: 'umd', name: 'k-routes'},
+    plugins: [
+      replace({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      }),
+      babel({
+        exclude: ['node_modules/**'],
+      }),
+      nodeResolve({
+        jsnext: true,
+      }),
+      commonjs({
+        namedExports: {
+          'node_modules/react/index.js': [
+            'createFactory',
+            'Children',
+            'Component',
+            'PropTypes',
+            'createElement',
+            'cloneElement',
+          ],
+          'node_modules/react-dom/index.js': ['render'],
+        },
+      }),
+    ],
+  },
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // the `targets` option which can specify `dest` and `format`)
+  {
+    input: 'src/main.js',
+    external: [
+      'ramda',
+      'react',
+      'reselect',
+      'k-reducer',
+      'redux',
+      'recompose',
+      'redux-actions',
+      'path-parser',
+      'react-redux',
+    ],
+    output: [{file: pkg.main, format: 'cjs'}, {file: pkg.module, format: 'es'}],
+    plugins: [
+      babel({
+        exclude: ['node_modules/**'],
+      }),
+    ],
+  },
 ];
